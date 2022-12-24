f648556accbb676e9e632912f0da7f7aa4a365cd
fhudint <fhud.int@gmail.com>
2022-12-24 21:47:34 +0700

    add xls utils

---
+++ EPyTools.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+
+"""
+EPyTools: This is an acronym for "EmisPendis Python Automation Tools Engine" 
+which is a cool name for a project using Python Selenium to automate tasks 
+that are typically done manually on the e-mispendis Ministry of Religion 
+application using the Python engine, 
+such as filling out forms or clicking buttons.
+"""
+
+from selenium import webdriver
---
+++ utils/xlsutils.py
@@ -0,0 +1,26 @@
+import openpyxl
+
+
+def getRowCount(file, sheetName):
+    workbook = openpyxl.load_workbook(file)
+    sheet = workbook.get_sheet_by_name(sheetName)
+    return (sheet.max_row)
+
+
+def getColumnCount(file, sheetName):
+    workbook = openpyxl.load_workbook(file)
+    sheet = workbook.get_sheet_by_name(sheetName)
+    return (sheet.column_max)
+
+
+def readData(file, sheetName, rownum, columnchar):
+    workbook = openpyxl.load_workbook(file)
+    sheet = workbook.get_sheet_by_name(sheetName)
+    return sheet.cell(row=rownum, column=columnchar).value
+
+
+def writeData(file, sheetName, rownum, columnchar, data):
+    workbook = openpyxl.load_workbook(file)
+    sheet = workbook.get_sheet_by_name(sheetName)
+    sheet.cell(row=rownum, column=columnchar).value = data
+    workbook.save(file)
